d11a446d682a2d8a8ac30a9902443c875b44f32a
Luciano Yomayel <luciano.yomayel@alumnos.frgp.utn.edu.ar>
2023-05-04 19:32:43 -0300

    listo

--- src/cursos/dto/update-curso.dto.ts
+++ src/cursos/dto/update-curso.dto.ts
@@ -1,33 +1,39 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCursoDto } from './create-curso.dto';
-import { IsArray, IsEnum, IsNotEmpty, IsNumber, IsPositive, Max, Min,IsString, IsMongoId } from 'class-validator';
+import { IsArray, IsEnum, IsNotEmpty, IsNumber, IsPositive, Max, Min,IsString, IsMongoId, IsEmpty, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { ETurno } from 'src/horario-x-curso/entities/horario-x-curso.entity';
 
 export class UpdateCursoDto {
+  @IsOptional()
   @IsString({message: 'Las notas deben ser una cadena de caracteres.'})
-  notas: string;
-
+  @ApiProperty({ description: 'Notas del curso' })
+  notas?: string;
+  
+  @IsOptional()
   @IsNumber({},{ message: 'El año debe ser un numero' })
   @IsPositive({ message: 'El año debe ser positivo'})
   @Min(1, {message: 'El año debe ser mayor o igual a 1'})
   @Max(6, {message: 'El año debe ser menor o igual a 6'})
   @ApiProperty({ description: 'Año del curso' })
-  readonly anio: number;
-
+  anio?: number;
   
+  
+  @IsOptional()
   @IsNumber({},{ message: 'La división debe ser un numero' })
   @IsPositive({ message: 'La división debe ser positiva'})
   @Min(1, {message: 'La división debe ser mayor o igual a 1'})
   @Max(6, {message: 'La división debe ser menor o igual a 6'})
-  readonly division: number;
-
+  @ApiProperty({ description: 'Division del curso' })
+  division?: number;
   
+  
+  @IsOptional()
   @IsArray({message: 'Los turnos deben estar en formato de array'})
   @ApiProperty({
-    description: 'Turno',
-    example: 'Turno mañana, Turno tarde, prehora,etc',
-  })
-  readonly turno: ETurno;
+      description: 'Turno',
+      example: 'Turno mañana, Turno tarde, prehora,etc',
+    })
+  turno?: ETurno;
   
 }
